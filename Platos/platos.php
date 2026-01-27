ef3fc7e3f9a6f1a54e12abb374d0cd268e66ac4c
DekFB <fbdek9051@gmail.com>
2026-01-27 09:22:57 +0100

    Add files via upload

--- Platos/platos.php
+++ Platos/platos.php
@@ -1,61 +1,57 @@
+<?php
+if (isset($_POST["Enviar"])) {
+
+    /*
+    $conexion = mysqli_connect("localhost", "root", "", "lindavista")
+        or die("No se puede conectar o seleccionar la base de datos");
+
+    $nombre = $_POST["Nombre"];
+    $descripcion = $_POST["Descripcion"];
+    $precio = $_POST["Precio"];
+
+    $sql = "INSERT INTO platos (nombre, descripcion, precio)
+            VALUES ('$nombre', '$descripcion', '$precio')";
+
+    $resultado = mysqli_query($conexion, $sql);
+
+    echo $resultado
+        ? "<p style='color:green;'>El plato se agregó correctamente.</p>"
+        : "<p style='color:red;'>Hubo un error al agregar el plato.</p>";
+
+    mysqli_close($conexion);
+    */
+
+    echo "<p style='color:green;'>El plato se agregó correctamente.</p>";
+
+} else {
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
     <title>Formulario de inserción de platos</title>
 </head>
 
 <body>
+    <div id="formulario">
+    <form action="" method="post">
+        <h1>Inserción de platos</h1>
+        <label>Nombre:</label>
+        <input type="text" name="Nombre" required><br><br>
 
-    <?php
-    /*
-    // Conectar con MySQL
-    $conexion = mysqli_connect("localhost", "root", "", "lindavista") 
-        or die("No se puede conectar o seleccionar la base de datos");
-    */
-    ?>
+        <label>Descripcion:</label>
+        <input type="text" name="Descripcion" required><br><br>
 
-    <?php if ($_SERVER["REQUEST_METHOD"] != "POST"): ?>
+        <label>Precio:</label>
+        <input type="number" name="Precio" min="0" required><br><br>
 
-        <form action="" method="post" enctype="multipart/form-data">
-            <label>Nombre:</label>
-            <input type="text" name="Nombre" required><br><br>
-
-            <label>Descripcion:</label>
-            <input type="text" name="Descripcion" required><br><br>
-
-            <label>Precio:</labe l>
-            <input type="number" name="Precio" min="0" required><br><br>
-
-            <button type="submit" name="Añadir">Añadir</button>
-        </form>
-
-    <?php else: ?>
-
-        <?php
-        /*
-        // Recoger datos
-        $nombre = $_POST["Nombre"];
-        $Descripcion = $_POST["Descripcion"];
-        $precio = $_POST["Precio"];
-
-        $sql = "INSERT INTO `platos` (`nombre`, `descripcion`, `precio`)
-                VALUES ('$nombre','$descripcion','$precio')";
-        $resultado = mysqli_query($conexion, $sql);
-
-        echo $resultado 
-            ? "<p style='color:green;'>El plato se agregó correctamente.</p>"
-            : "<p style='color:red;'>Hubo un error al agregar el plato.</p>";
-
-        mysqli_close($conexion);
-        */
-        echo "<p style='color:green;'>El plato se agregó correctamente.</p>";
-        ?>
-
-
-    <?php endif; ?>
-
+        <input type="submit" name="Enviar"></button>
+    </form>
+    </div>
 </body>
-
 </html>
+<?php
+}
+?>
